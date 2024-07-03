2cf53e23d3ed84453a84968bfe2906223ee7b5b6
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-03 09:49:45 +0530

    tst

--- Read&Write/Read&Write - path/readwritesync2.js
+++ Read&Write/Read&Write - path/readwritesync2.js
@@ -1,6 +1,5 @@
 // const fs = require('fs')
 // const path = require('path')
-// console.log(path.join(__dirname))
 // let data = fs.readFileSync(path.join(__dirname,'C:/Users/DELL/Desktop/Programming/Node js/Read&Write/data.txt'),'utf-8')
 // fs.writeFileSync('Hyd.txt',data,'utf-8')
 // console.log(data)
@@ -8,15 +7,6 @@
 
 const fs = require('fs');
 const path = require('path');
-
-// Construct the full path to the data.txt file
-const dataFilePath = path.join('C:/Users/DELL/Desktop/Programming/Node js/Read&Write', 'data.txt');
-
-// Read the content of the data.txt file
-let data = fs.readFileSync(dataFilePath, 'utf-8');
-
-// Write the content to Hyd.txt in the current directory
+let data = fs.readFileSync(path.join('C:/Users/DELL/Desktop/Programming/Node js/Read&Write', 'data.txt'), 'utf-8');
 fs.writeFileSync(path.join(__dirname, 'Hyd.txt'), data, 'utf-8');
-
-// Log the content to the console
 console.log(data);
