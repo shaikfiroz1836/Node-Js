ae92b641748f7e301e1386f9d3461c8cdb008abc
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-05 09:51:18 +0530

    'tst'

--- HTTP/HTTP Static/server.js
+++ HTTP/HTTP Static/server.js
@@ -1,8 +1,14 @@
 const http = require('http')
+const fs = require('fs')
 let server = http.createServer((req,res)=>{
 
+    fs.readFile('index.html','utf-8',(err,data)=>{
+        if(err) throw err
+        res.end(data)
+        console.log(req.url)
+    })
 
-    res.end("<h1>Hello! Good Morning</h1>")
+    
 })
 
 server.listen(3000,'127.0.0.1',(err)=>{
