5d868e8888e95b8856f66d52f3d21a714d2a82c8
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-09 09:27:08 +0530

    'tst'

--- 3rd party or dependencies/Nodemon/server.js
+++ 3rd party or dependencies/Nodemon/server.js
@@ -5,5 +5,13 @@ dotenv.config({ path: './config/.env' })
 
 let port = process.env.PORT
 let host_name = process.env.HOST_NAME
-console.log(port)
-console.log(host_name)
+// console.log(port)
+// console.log(host_name)
+
+let server = http.createServer((req,res)=>{
+    res.end("HTTP Server")
+})
+server.listen(port,host_name,(err)=>{
+    if(err) throw err
+    console.log(`Server is running on http://${host_name} and ${port}`)
+})
