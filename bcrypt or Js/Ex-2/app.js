7ed8e50cbe369cbf94d8c40c8556c57a307b1f47
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-12 09:37:51 +0530

    tst

---
+++ bcrypt or Js/Ex-2/app.js
@@ -0,0 +1,20 @@
+import bcrypt from'bcrypt'
+
+let user = {
+    name:'Rahul',
+    email:'Rahul123@gmail.com',
+    cc:'1234 1234 1234 1111',
+    cvv:'121',
+    password:'Prostack123'
+}
+
+let salt = bcrypt.genSaltSync(10)
+
+let new_cc = bcrypt.hashSync(user.cc,salt)
+let new_password = bcrypt.hashSync(user.password,salt)
+
+console.log(user.cc)
+console.log(new_cc)
+
+user = {...user,cc:new_cc,password:new_password}
+console.log(user)
