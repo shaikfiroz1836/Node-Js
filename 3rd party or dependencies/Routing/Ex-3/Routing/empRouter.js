b196fa1fbe1f9e175a4f015e0bda823131291d41
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-16 09:42:11 +0530

    tst

---
+++ 3rd party or dependencies/Routing/Ex-3/Routing/empRouter.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import fs from 'fs'
+
+let router = express.Router()
+
+router.get('/read',async(req,res)=>{
+    let employees = await getEmployees()
+    res.status(200).json(employees)
+})
+router.post('/create',(req,res)=>{
+    let emp = req.body
+    console.log(emp)
+})
+router.put('/update',(req,res)=>{
+    
+    res.json({"msg":"Good Morning"})
+
+})
+router.delete('/delete/:id',(req,res)=>{
+    res.send('Delete Request')
+})
+
+let saveEmployees=(employees)=>{}
+let getEmployees=()=>{
+    let employees = fs.readFileSync('data.json','utf-8')
+    return JSON.parse(employees);
+}
+
+export default router;
