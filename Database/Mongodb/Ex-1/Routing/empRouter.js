925661c37d64bea71ec0c7b33612b411cb266453
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-19 09:02:38 +0530

    tst

--- Database/Mongodb/Ex-1/Routing/empRouter.js
+++ Database/Mongodb/Ex-1/Routing/empRouter.js
@@ -3,8 +3,18 @@ import Employee from '../Model/employee.js'
 let router = express.Router()
 
 router.get('/read',async(req,res)=>{
-    let employees = await Employee.find()
+    try{
+        let employees = await Employee.find()
     res.status(200).json(employees)
+    }catch{
+        res.status(500).json({message:'Internal Server Error'})
+    }
+    
 })
+
+router.post('/create',(req,res)=>{
+    
+})
+
 
 export default router;
