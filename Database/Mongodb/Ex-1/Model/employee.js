228be658c8a31a5117fe278b107390b14ffa5eb8
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-20 09:40:28 +0530

    tst

--- Database/Mongodb/Ex-1/Model/employee.js
+++ Database/Mongodb/Ex-1/Model/employee.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import {type} from 'os'
 
 let emp_Schema=mongoose.Schema({
-    eid:{type:Number, require:true},
-    ename:{type:String,require:true},
-    esal:{type:Number,require:true}
+    id:{type:Number, require:true},
+    name:{type:String,require:true},
+    sal:{type:Number,require:true}
 })
 let Employee=mongoose.model("employee",emp_Schema)
 export default Employee;
--- Database/Mongodb/Ex-1/Routing/empRouter.js
+++ Database/Mongodb/Ex-1/Routing/empRouter.js
@@ -1,29 +1,48 @@
-import express from 'express'
-import Employee from '../Model/employee.js'
-let router = express.Router()
+import express from "express";
+import EmployeeModel from '../Model/employee.js'
+let router=express.Router();
 
-router.get('/read',async(req,res)=>{
+//URL:localhost:8080/emp/
+//Method:GET
+router.get("/",(req,resp)=>{
+
+    resp.status(200).json({"msg":"Employee Root Request"})
+})
+
+//URL:localhost:8080/emp/read
+//GET:GET
+router.get("/read", async (req,resp)=>{
     try{
-        let employees = await Employee.find()
-    res.status(200).json(employees)
-    }catch{
-        res.status(500).json({message:'Internal Server Error'})
+        let employees= await EmployeeModel.find();
+        return resp.status(200).json(employees);
     }
-    
-})
-
-router.post('/create',(req,res)=>{
-    let emp = req.body
-    console.log(emp)
-    let Employee = EmployeeModel.findOne({eid:emp.eid})
-    console.log(Employee)
-    if(Employee){
-        return res.status(200).json({"msg":"Employee Already Exists"})
+    catch(err){
+        return resp.status(401).json({"error message":err.message})
     }
-    employee = new EmployeeModel(emp)
-    employee.save()
-    
+   
 })
+/*
+    Usage: create new employee
+    URL:http://127.0.0.1:8080/emp/create
+    Method:POST
+    Req Fields:eid,ename,esal
+*/
+router.post("/create",async (req,resp)=>{
+    try{
+        let emp = req.body;
+        let Employee=await EmployeeModel.findOne({id:emp.id})
+        console.log(Employee)
+        if(Employee){
+            return resp.status(401).json({"msg":"Employee already exits"})
+        }
+        Employee=new EmployeeModel(emp)
+        console.log(Employee)
+        await Employee.save();
+        return resp.status(200).json({"msg":"Employee created Successufully"})
 
-
+    }catch(err){
+        return resp.status(401).json({"error message":err.message})
+    }
+       
+})
 export default router;
