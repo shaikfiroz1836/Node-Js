9bc6a174a5d5176b5449cd813d307110df842643
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-31 10:22:26 +0530

    tst

--- Project - Rest Api/routes/Router.js
+++ Project - Rest Api/routes/Router.js
@@ -6,4 +6,11 @@ router.get('/read',(req,res)=>{
     res.status(200).json({"msg":"Read Request"})
 })
 
+router.post('/create',(req,res)=>{
+    let data = []
+    data.push(req.body)
+    res.status(200).json({"msg":"Data Created Succesfully..."})
+    res.status(200).json(data)
+})
+
 export default router;
