40469e62debc9cb61af0caa5315adfce7f598e8b
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-07-31 16:51:10 +0530

    tst

--- Project - Rest Api/routes/Router.js
+++ Project - Rest Api/routes/Router.js
@@ -8,9 +8,12 @@ router.get('/read',(req,res)=>{
 
 router.post('/create',(req,res)=>{
     let data = []
-    data.push(req.body)
-    res.status(200).json({"msg":"Data Created Succesfully..."})
-    res.status(200).json(data)
+    let {body} = req;
+    if(!body.name){
+       return res.status(400).json({"msg":"Name is required"})
+    }
+    return res.status(200).json({"msg":"Data Created Succesfully..."})
+
 })
 
 export default router;
