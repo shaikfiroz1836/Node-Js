44ca19729006c9505382df4c8b14eef1ee1d1687
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-08-05 12:47:37 +0530

    tst

--- Project - Rest Api/routes/Router.js
+++ Project - Rest Api/routes/Router.js
@@ -22,8 +22,47 @@ router.post('/create', async (req, res) => {
 
     employees.push(emp_data);
     await saveEmployees(employees);
-    return res.status(200).json({"Msg": "Employee created successfully!"});
+    return res.status(200).json({"Msg": "Employee created successfully"});
 });
+
+// router.put('/update/:id',async(req,res)=>{
+//     let emp_id = req.params.id;
+//     let emp_data = req.body;
+//     let employees =await getEmployees();
+
+//     let flag = employees.findIndex((emp)=>{
+//         console.log(emp.id)
+//         console.log(emp_id)
+//         return emp.id === emp_id;
+//     })
+//     if(!flag){
+//         return res.status(401).json({"msg":"Employee Not Exists With Id!"})
+//     }
+//     else{
+//         employees[emp_id] = { ...employees[emp_id], ...emp_data };
+//         await saveEmployees(employees)
+//     return res.status(200).json({"msg":"Employee Updated Successfully"})
+//     }
+// })
+
+router.put("/update/:id",async(req,resp)=>{
+    let emp_Id=req.params.id;
+    let emp_Obj = req.body;
+    let employees = await getEmployees()
+    let emp=employees.find((emp)=>{
+        return emp.eid==emp_Id
+    })
+    if(!emp){
+        return resp.status(401).json({"msg":"Employee Not Exists"})
+    }
+    let remaining_Employees=employees.filter((emp)=>{
+        return emp.eid !=emp_Id;
+    })
+    remaining_Employees.unshift(emp_Obj)
+    saveEmployees(remaining_Employees)
+    return resp.status(200).json({"msg":"Employee Object updated Successfully"})
+})
+
 
 let getEmployees = () => {
     let employees = fs.readFileSync('data.json', 'utf-8');
